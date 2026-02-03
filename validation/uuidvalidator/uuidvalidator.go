6104c6a444df87defcc57ea8b28d5c6a1e0ed491
Davi Rezende <davi.rezende@grupoboticario.com.br>
2026-02-03 10:30:39 -0300

    fix

--- validation/uuidvalidator/uuidvalidator.go
+++ validation/uuidvalidator/uuidvalidator.go
@@ -11,6 +11,8 @@ var (
 	// UUID format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 	uuidRegexp = `[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}`
 
+	uuidCompiledRegexp = regexp.MustCompile(`^` + uuidRegexp + `$`)
+
 	// SAP BTP short subaccount ID
 	// Exactly 8 lowercase alphanumeric characters, starting with a letter
 	subaccountShortRegexp = `[a-z][a-z0-9]{7}`
@@ -20,6 +22,14 @@ var (
 		`^(` + uuidRegexp + `|` + subaccountShortRegexp + `)$`,
 	)
 )
+
+// ValidUUID validates that the value is a UUID
+func ValidUUID() validator.String {
+	return stringvalidator.RegexMatches(
+		uuidCompiledRegexp,
+		"value must be a valid UUID",
+	)
+}
 
 // ValidSubaccountID validates that the value is either a UUID or a SAP BTP short subaccount ID
 func ValidSubaccountID() validator.String {
